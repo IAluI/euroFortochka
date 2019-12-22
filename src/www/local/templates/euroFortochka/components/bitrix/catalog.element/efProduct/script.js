e4848c1b0a048f5bf5f7c43e9aee8f34cd3d9a8d
IAluI <29124424+ialui@users.noreply.github.com>
2019-12-23 01:17:37 +0700

    -Страница "Оборудование"

--- src/www/local/templates/euroFortochka/components/bitrix/catalog.element/efProduct/script.js
+++ src/www/local/templates/euroFortochka/components/bitrix/catalog.element/efProduct/script.js
@@ -11,4 +11,17 @@ $(document).ready(function() {
     spaceBetween: 50,
     touchEventsTarget: 'wrapper',
   });
+
+  var productOffers = new Swiper('.Product-Offers', {
+    a11y: {
+      enabled: false
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 50,
+    touchEventsTarget: 'wrapper',
+  });
 });
--- src/www/local/templates/euroFortochka/components/bitrix/catalog.element/efProduct/template.php
+++ src/www/local/templates/euroFortochka/components/bitrix/catalog.element/efProduct/template.php
@@ -2,9 +2,9 @@
   if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 
-<div class="" id="<?=$itemIds['ID']?>" itemscope itemtype="http://schema.org/Product">
-  <div class="row">
-    <div class="col-md-6">
+<div class="Product" id="<?=$itemIds['ID']?>" itemscope itemtype="http://schema.org/Product">
+  <div class="row mb-3">
+    <div class="mb-3 col-md-6">
       <div class="Product-Images">
         <div class="swiper-container Product-MainImages">
           <div class="swiper-wrapper">
@@ -13,7 +13,7 @@
             ?>
               <div class="swiper-slide">
                 <div>
-                  <div style="background-image: url(<?=CFile::GetFileArray($pictureId)['SRC'];?>);"></div>
+                  <div style="background-image: url(<?= CFile::GetFileArray($pictureId)['SRC']; ?>);"></div>
                 </div>
               </div>
             <?
@@ -28,6 +28,47 @@
     <div class="col-md-6 Product-ShortChar">
       <?=$arResult['PROPERTIES']['short_characteristics']['~VALUE']['TEXT']?>
     </div>
+  </div>
+  <div itemprop="offers" itemscope itemtype="http://schema.org/AggregateOffer" class="swiper-container mb-3 Product-Offers">
+    <div class="swiper-wrapper">
+      <?
+        $offerNames = &$arResult['PROPERTIES']['configuration_option_names']['VALUE'];
+        $offerImg = &$arResult['PROPERTIES']['configuration_option_pictures']['VALUE'];
+        $offerDesc = &$arResult['PROPERTIES']['configuration_option_desc']['~VALUE'];
+        $offerPrices = &$arResult['PROPERTIES']['configuration_option_prices']['VALUE'];
+        for ($i = 0; $i < count($offerNames); $i++):
+      ?>
+        <div class="swiper-slide p-3 col-md-4 col-xl-3 Product-Offer">
+          <div class="rounded-top shadow h-100 overflow-hidden">
+            <div class="p-2 bg-primary Product-OfferHeader">
+              <div>
+                <?= $offerNames[$i]; ?>
+              </div>
+            </div>
+            <div class="m-2 Product-OfferImg">
+              <img alt="<?= $offerNames[$i]; ?>" src="<?= CFile::GetFileArray($offerImg[$i])['SRC']; ?>">
+            </div>
+            <div class="mx-2 mb-3 Product-OfferDesc">
+              <div>
+                <?= $offerDesc[$i]; ?>
+              </div>
+            </div>
+            <p class="py-2 mx-2 border-top border-bottom font-weight-bold Product-OfferPrice">
+              <?= (number_format($offerPrices[$i], 0, '.', ' ') . ' Р'); ?>
+            </p>
+            <div class="mb-3 Product-OfferImgBtn">
+              <button class="btn btn-secondary font-weight-bold text-white Callback" type="button">
+                Купить
+              </button>
+            </div>
+          </div>
+        </div>
+      <?
+        endfor;
+      ?>
+    </div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
   </div>
   <pre>
     <?
